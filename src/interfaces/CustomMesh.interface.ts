43bcce448d448527e77f4134871f4ee7885600b9
Jonathan Adithya <jo.adithya391@gmail.com>
2023-12-13 02:40:11 -0800

    feat: create a custom mesh interface

---
+++ src/interfaces/CustomMesh.interface.ts
@@ -0,0 +1,23 @@
+import * as THREE from "three";
+
+import { Experience } from "Experience";
+
+export abstract class CustomMesh extends THREE.Mesh {
+  protected experience = Experience.getInstance();
+
+  constructor() {
+    super();
+
+    this.setGeometry();
+    this.setMaterial();
+    this.setMesh();
+
+    this.experience.add(this);
+  }
+
+  abstract setGeometry(): void;
+  abstract setMaterial(): void;
+  setMesh() {}
+
+  update() {}
+}
